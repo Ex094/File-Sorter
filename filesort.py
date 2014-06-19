47d13a122e063788c4963ce1c3bc15f1f18806d7
Ex094 <whitehat94@live.com>
2014-06-19 21:11:37 +0500

    Updated
    Added Command Line support for path, Easy to sort folders in different
    directory instead of placing the script in the folder.

--- filesort.py
+++ filesort.py
@@ -1,45 +1,46 @@
-import os, shutil
+import os, shutil, sys
 
-###Path where the script is located
-###Do not change anything
-path = os.path.dirname(os.path.abspath("filesort.py"))
-files = os.listdir(path)
-
-
-###Extensions and their respective folder
-###names
+###Extensions and their respective folder names
 extension = {'Text': '.txt', 'Executables': '.exe', 'Python Executable': '.py'}
 
-###Check if folder is not empty
-if (files == 0):
-    print "No files found in this directory"
-    
-else:
 
-    ###Create Folders
-    for name in extension:
-        try:
-            os.makedirs(name)
+def file_sort(path):
+    ###Path where the script is located
+    files = os.listdir(path)
 
-    ###If Folder already exists then ignore
-        except:
-            pass
-        
-    for ex in extension:
+    ###Check if folder is not empty
+    if (files == 0):
+        print "No files found in this directory"
+
+    else:
+        for ex in extension:
             for item in files:
                 if item.endswith(extension[ex]):
                     try:
 
                         ###If Folder exists then move file
-                        if os.path.isdir(ex):
-                            shutil.move(item, ex)
+                        if os.path.isdir(path + ex):
+                            shutil.move(path + item, path + ex)
 
                         ###Else create folder then move file
                         else:
-                            os.makedirs(ex)
-                            shutil.move(item, ex)
+                            os.makedirs(path + ex)
+                            shutil.move(path + item, path + ex)
 
                     ###Error handler in case if files already exist
                     except:
                         print "Files And Folders have been sorted already!"
                         break
+
+
+try:
+    if(sys.argv[1]):
+        if(os.path.isdir(sys.argv[1])):
+           print "Sorting files in defined path: " + sys.argv[1]
+           file_sort(sys.argv[1])
+           
+        else:
+           print "Directory does not exist"
+except:
+    print "No Directory or Path Defined!"
+    print "Command: filesort.py C:/folder/folder"
